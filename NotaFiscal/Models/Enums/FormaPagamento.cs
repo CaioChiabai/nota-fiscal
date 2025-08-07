618e072e6327893e2fe1b834088b63df1526f60d
Caio Chiabai <caiochiabaioliveira@gmail.com>
2025-08-07 14:40:53 -0300

    Criado enum tipoendereco e ajustado enum formapagamento

--- NotaFiscal/Models/Enums/FormaPagamento.cs
+++ NotaFiscal/Models/Enums/FormaPagamento.cs
@@ -3,10 +3,8 @@
     public enum FormaPagamento
     {
         Dinheiro = 1,
-        CartaoCredito = 2,
-        CartaoDebito = 3,
-        Pix = 4,
-        TransferenciaBancaria = 5,
-        Boleto = 6
+        Cartao = 2,
+        Pix = 3,
+        Boleto = 4
     }
 }
---
+++ NotaFiscal/Models/Enums/TipoEndereco.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotaFiscal.Models.Enums
+{
+    public enum TipoEndereco
+    {
+        Cobranca = 1,
+        Entrega = 2
+    }
+}
