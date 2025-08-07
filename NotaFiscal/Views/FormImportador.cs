87a4f0871e994d8a822322ffa60f1407c2403ccb
Caio Chiabai <caiochiabaioliveira@gmail.com>
2025-08-07 15:24:27 -0300

    Configurado evento do botao importar para chamar controller

--- NotaFiscal/Views/FormImportador.cs
+++ NotaFiscal/Views/FormImportador.cs
@@ -1,11 +1,17 @@
-﻿namespace NotaFiscal.Views
+﻿using NotaFiscal.Controllers;
+
+namespace NotaFiscal.Views
 {
     public partial class FormImportador : Form
     {
-        public FormImportador()
+        private readonly PlanilhaController _planilhaController;
+
+        public FormImportador(PlanilhaController planilhaController)
         {
             InitializeComponent();
+            _planilhaController = planilhaController;
         }
+
 
         private void btnSelecionarPlanilha_Click(object sender, EventArgs e)
         {
@@ -17,7 +23,31 @@
 
         private void btnImportarPlanilha_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(txtBoxCaminhoPlanilha.Text))
+            {
+                MessageBox.Show("Por favor, selecione uma planilha para importar.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
 
+            try
+            {
+                btnImportarPlanilha.Enabled = false;
+                btnImportarPlanilha.Text = "Importando...";
+
+                _planilhaController.ImportarPlanilha(txtBoxCaminhoPlanilha.Text);
+
+                MessageBox.Show("Planilha importada com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ocorreu um erro ao importar a planilha: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                btnImportarPlanilha.Enabled = true;
+                btnImportarPlanilha.Text = "Importar";
+                txtBoxCaminhoPlanilha.Text = string.Empty;
+            }
         }
     }
 }
